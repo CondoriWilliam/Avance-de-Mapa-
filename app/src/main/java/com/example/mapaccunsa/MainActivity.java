388314692f89ee3ce0c0ff597b20787b682bd46a
CondoriWilliam <wcondorian@unsa.edu.pe>
2024-06-09 18:51:17 -0500

    Implementacion del mapa

--- app/src/main/java/com/example/mapaccunsa/MainActivity.java
+++ app/src/main/java/com/example/mapaccunsa/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.mapaccunsa;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.mapaccunsa.fragments.CuadrosFragment;
+import com.example.mapaccunsa.fragments.HomeFragment;
+import com.example.mapaccunsa.fragments.MapaFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    MapView mapView;
+    private FragmentManager fragmentManager = null;
+    private FragmentTransaction fragmentTransaction = null;
+    private HomeFragment homeFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        mapView = new MapView(this);
-        setContentView(mapView);
+        fragmentManager = getSupportFragmentManager();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.menu_home){
+                    homeFragment = HomeFragment.newInstance("","");
+                    loadFragment(homeFragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.menu_cuadros){
+                    loadFragment(new CuadrosFragment());
+                    return true;
+                } else if (menuItem.getItemId() == R.id.menu_mapa){
+                    loadFragment(new MapaFragment());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+        if (fragmentManager != null){
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+            fragmentTransaction.commit();
+        }
     }
 }
